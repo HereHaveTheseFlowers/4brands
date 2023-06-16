613ea6bcd5e73ea89666452789b2ec3bc73cf6ec
herehavetheseflowers <herehavetheseflowers@gmail.com>
2023-06-16 16:43:12 +0300

    Homepage setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://herehavetheseflowers.github.io',
-    base: '/4brands',
+    base: './',
   })
